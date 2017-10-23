aaa3cb2d379ef63d3f59cec984a6cd45567d68e2
Kevin Seagraves <k.s.seagraves@gmail.com>
2017-10-23 11:58:52 -0700

    tests passing, events not emitted on passes, only when failing...

--- test/Blox.js
+++ test/Blox.js
@@ -2,14 +2,13 @@ var Blox = artifacts.require("./Blox.sol");
 
 contract('Blox', function(accounts) {
 
-  let ticketPrice = "52343.95";
-  let eventName = "BCGDV";
-  let eventLocation = "Manhattan Beach";
-  let eventDescription = "Nice office building near the beach";
-
-  it("should have data in each field", async function() {
-
+  it("should have data in each field for first event", async function() {
     let instance = await Blox.new({from: accounts[0]});
+
+    let ticketPrice = "52343.95";
+    let eventName = "BCGDV";
+    let eventLocation = "Manhattan Beach";
+    let eventDescription = "Nice office building near the beach";
 
     await instance.createBlox(ticketPrice, eventName, eventLocation, eventDescription);
 
@@ -18,13 +17,41 @@ contract('Blox', function(accounts) {
     let tp = selectedEvent[1];
     assert.isTrue(tp == 52343.95);
 
-    // let en = await instance.eventName.call();
-    // assert.isTrue(en == "BCGDV");
-    //
-    // let el = await instance.eventLocation.call();
-    // assert.isTrue(el == "Manhattan Beach");
-    //
-    // let ed = await instance.eventDescription.call();
-    // assert.isTrue(ed == "Nice office building near the beach");
+    let en = await selectedEvent[2];
+    assert.isTrue(en == "BCGDV");
+
+    let el = await selectedEvent[3];
+    assert.isTrue(el == "Manhattan Beach");
+
+    let ed = await selectedEvent[4];
+    assert.isTrue(ed == "Nice office building near the beach");
   });
+
+  it("should have data in each field for second event", async function() {
+
+    let instance = await Blox.deployed();
+
+    let ticketPrice = "400";
+    let eventName = "DVolution";
+    let eventLocation = "World Wide";
+    let eventDescription = "On the Chain";
+
+    await instance.createBlox(ticketPrice, eventName, eventLocation, eventDescription);
+
+    let selectedEvent = await instance.getBlox.call(1);
+
+    let tp = selectedEvent[1];
+    assert.isTrue(tp == "400");
+
+    let en = await selectedEvent[2];
+    assert.isTrue(en == "DVolution");
+
+    let el = await selectedEvent[3];
+    assert.isTrue(el == "World Wide");
+
+    let ed = await selectedEvent[4];
+    assert.isTrue(ed == "On the Chain");
+  });
+
+
 })
